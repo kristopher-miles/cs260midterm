2a33ffefc2629c554251b416952dab5e40827c20
Kristopher Miles <kristophermiles@gmail.com>
2017-11-27 22:24:01 +0000

    First commit. starting from basic project.

---
+++ public/javascripts/frontApp.js
@@ -0,0 +1,44 @@
+angular.module('comment', []).controller('MainCtrl',[
+	'$scope','$http',
+	function($scope,$http){
+		$scope.comments = [];
+		$scope.addComment = function(){
+			var newObject = {title:$scope.formContent,upvotes:0};
+			
+			$scope.create(newObject);
+		};
+		
+		  $scope.create = function(comment) {
+    			return $http.post('/comments', comment).success(function(data){
+      			$scope.comments.push(data);
+    			});
+  		  };
+		
+		    $scope.upvote = function(comment) {
+      return $http.put('/comments/' + comment._id + '/upvote')
+        .success(function(data){
+          console.log("upvote worked");
+          comment.upvotes += 1;
+        });
+    };   
+		
+		$scope.incrementUpvotes = function(comment){
+			$scope.upvote(comment);
+		};
+
+		    $scope.delete = function(comment) {
+      $http.delete('/comments/' + comment._id )
+        .success(function(data){
+          console.log("delete worked");
+        });
+      $scope.getAll();
+    };
+		
+		$scope.getAll = function() {
+   			 return $http.get('/comments').success(function(data){
+     				 angular.copy(data, $scope.comments);
+    			});
+  		};
+  		$scope.getAll();	
+	}
+]);
